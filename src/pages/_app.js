5cd1d3f2150cb244800296621533ee6760ff8633
Md. Rakib <113634586+0xmdrakib@users.noreply.github.com>
2025-05-08 17:28:20 +0600

    Create _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,31 @@
+import '../tailwind.css';
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Nexora',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
